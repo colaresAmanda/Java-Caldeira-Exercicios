85b75407f3185a2c5bd878282b658c386aacf196
Amanda Colares <colares.amd@gmail.com>
2023-10-12 08:54:01 -0300

    feat: Implement exercise 2 solution for lecture 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 import com.colaresamanda.aula01.exercicio01.PrincipalA01E01;
+import com.colaresamanda.aula01.exercicio02.PrincipalA01E02;
 
 public class Main {
     public static void main(String[] args) {
-        var ex1 = new PrincipalA01E01();
-        ex1.run();
+        //  Exercício 01:  Crie um programa que leia seu nome e imprima na tela.
+        var A01E01 = new PrincipalA01E01();
+        A01E01.run();
+
+        //   Exercício 02: Crie um programa que receba 2 números e imprima na tela.
+        var A01E02 = new PrincipalA01E02();
+        A01E02.run();
+
+        
     }
 }
--- src/com/colaresamanda/aula01/exercicio02/PrincipalA01E02.java
+++ src/com/colaresamanda/aula01/exercicio02/PrincipalA01E02.java
@@ -1,4 +1,19 @@
 package com.colaresamanda.aula01.exercicio02;
 
+import java.util.Scanner;
+
 public class PrincipalA01E02 {
+    //    Crie um programa que receba 2 números e imprima na tela.
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter another number: ");
+        int number2 = scanner.nextInt();
+
+        System.out.printf("Number 1: %d\nNumber 2: %d", number1, number2);
+    }
 }
