f2bd5215435bbe1ff7ba66b94f6158bb675167f9
Amanda Colares <colares.amd@gmail.com>
2023-10-12 08:48:39 -0300

    feat: Implement exercise 1 solution for lecture 1

--- src/com/colaresamanda/aula01/exercicio01/PrincipalA01E01.java
+++ src/com/colaresamanda/aula01/exercicio01/PrincipalA01E01.java
@@ -1,7 +1,14 @@
 package com.colaresamanda.aula01.exercicio01;
 
+import java.util.Scanner;
+
 public class PrincipalA01E01 {
+    //    Crie um programa que leia seu nome e imprima na tela.
+
     public void run(){
-        System.out.println("It's running!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: " + name);
     }
 }
